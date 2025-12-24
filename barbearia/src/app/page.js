aa96918ef79a9800eb9a6bdb9ac781a9c74b9c45
MaiconSouzaDevFrontEnd <you@example.com>
2025-12-24 18:57:42 -0300

    reparo em logica no page.js

--- barbearia/src/app/page.js
+++ barbearia/src/app/page.js
@@ -1,10 +1,20 @@
 /* import Image from "next/image";
 import styles from "./page.module.css"; */
+'use client';
+import { useState } from "react";
 
 export default function Home() {
+  const [ temaEscuro, setTemaEscuro ] = useState (false);
+
+  const alternarTema = () => {
+    setTemaEscuro (!temaEscuro);
+  }
   return (
     <div >
-      
+
+      <button onClick={alternarTema}>
+        {temaEscuro ? "Modo Claro" : "Modo Escuro"}
+      </button>
     </div>
   );
 }
